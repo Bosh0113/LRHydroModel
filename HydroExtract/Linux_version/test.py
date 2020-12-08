c6d4267312eb2597ae75b3766d9549c3e8e30c7b
BeiChen Zhang <45487945+Bosh0113@users.noreply.github.com>
2020-12-08 15:13:39 +0800

    测试重叠数据导入和查询
    用于测试新数据集分流域计算后的合并

--- HydroExtract/Linux_version/test.py
+++ HydroExtract/Linux_version/test.py
@@ -1,35 +1,63 @@
 import os
 import time
-import gdal
-import common_utils as cu
-import direction_reclassify as dr
+import geopyspark as gps
+import json
+from pyspark import SparkContext
+from shapely.geometry import Polygon
+import clip_tif as ct
 
 
-def test(work_path):
+def test(work_path, filename):
 
-    # process_path = work_path + "/process"
-    # if not os.path.exists(process_path):
-    #     os.makedirs(process_path)
-    # result_path = work_path + "/result"
-    # if not os.path.exists(result_path):
-    #     os.makedirs(result_path)
+    process_path = work_path + "/process"
+    if not os.path.exists(process_path):
+        os.makedirs(process_path)
+    result_path = work_path + "/result"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
 
-    lake_path = work_path + "/lakes.tif"
-    lake_ds = gdal.Open(lake_path)
-    for i in range(lake_ds.RasterYSize):
-        for j in range(lake_ds.RasterXSize):
-            int_value = cu.get_raster_int_value(lake_ds, j, i)
-            unint_value = cu.get_raster_un_int_value(lake_ds, j, i)
-            if int_value != unint_value and int_value != -9:
-                print("signed int -> ", int_value)
-                print("unsigned int -> ", unint_value)
+    # catalog_path = '/usr/local/large_scale_hydro/catalog'
+    catalog_path = '/usr/local/large_scale_hydro/Test/4/catalog'
+    json_path = work_path + '/' + filename + '.geojson'
+    dem_tif_path = process_path + '/' + filename + '.tif'
+    catalog_path = "file://" + catalog_path
+
+    poly = None
+    # Creates a Polygon from Geojson
+    with open(json_path) as f:
+        js = json.load(f)
+        features = js['features']
+        if features[0]['geometry']['type'] == 'Polygon':
+            polygon = features[0]['geometry']['coordinates']
+            points = polygon[0]
+            input_array = []
+            for point in points:
+                input_array.append(tuple(point))
+            poly = Polygon(input_array)
+
+    print("Get DEM")
+    # tiled_raster_layer = gps.query(uri=catalog_path, layer_name="dem", layer_zoom=0, query_geom=poly)
+    tiled_raster_layer = gps.query(uri=catalog_path, layer_name="dem-demo", layer_zoom=0, query_geom=poly)
+    print(tiled_raster_layer.count())
+    print(tiled_raster_layer.layer_metadata.extent)
+    tiled_raster_layer.save_stitched(dem_tif_path)
+
+    print("Clip DEM")
+    dem_clip = result_path + "/" + filename + "_clip.tif"
+    ct.geojson_clip_tif(json_path, dem_tif_path, dem_clip)
 
     print("test")
 
 
 if __name__ == '__main__':
+    conf = gps.geopyspark_conf(master="local[*]", appName="master")
+    pysc = SparkContext(conf=conf)
+
     start = time.perf_counter()
-    workspace_path = "D:/Graduation/Program/Data/21"
-    test(workspace_path)
+    workspace_path = "/usr/local/large_scale_hydro/Test/4"
+    # files = ['data1', 'data2', 'data3']
+    files = ['query_area']
+    for file in files:
+        test(workspace_path, file)
     end = time.perf_counter()
     print('Run', end - start, 's')
--- HydroExtract/test.py
+++ HydroExtract/test.py
@@ -1,8 +1,6 @@
 import os
 import time
-import direction_reclassify as dr
-import land_ocean as lo
-import watershed_extract as we
+import clip_tif as ct
 
 
 def test(work_path):
@@ -14,30 +12,27 @@ def test(work_path):
     if not os.path.exists(result_path):
         os.makedirs(result_path)
 
-    acc_data = work_path + "/preprocess/acc.tif"
-    dir_data = work_path + "/preprocess/dir.tif"
-    dem_data = work_path + "/preprocess/dem.tif"
+    shp_data1 = work_path + "/preprocess/data1.shp"
+    geojson_data1 = process_path + "/data1.geojson"
+    ct.shp_to_geojson(shp_data1, geojson_data1)
+    shp_data2 = work_path + "/preprocess/data2.shp"
+    geojson_data2 = process_path + "/data2.geojson"
+    ct.shp_to_geojson(shp_data2, geojson_data2)
+    shp_data3 = work_path + "/preprocess/data3.shp"
+    geojson_data3 = process_path + "/data3.geojson"
+    ct.shp_to_geojson(shp_data3, geojson_data3)
+    shp_query = work_path + "/preprocess/query_area.shp"
+    geojson_query = process_path + "/query_area.geojson"
+    ct.shp_to_geojson(shp_query, geojson_query)
 
-    # 流向数据重分类
-    print("--------------------------------Reclassify Direction--------------------------------")
-    dir_reclass = process_path + "/dir_reclass.tif"
-    # dr.dir_reclassify(dir_data, dir_reclass)
 
-    # # 提取路上入海点
-    # print("-------------------------------------Get Seaside-------------------------------------")
-    final = process_path + "/final.tif"
-    # lo.get_seaside(dir_reclass, dir_data, seaside)
-    #
-    # # 提取子流域
-    # print("------------------------------------Get Watershed-----------------------------------")
-    we.get_watershed(result_path, dem_data, dir_reclass, acc_data, final)
 
     print("test")
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    workspace_path = "D:/Graduation/Program/Data/24"
+    workspace_path = "D:/Graduation/Program/Data/26"
     test(workspace_path)
     end = time.perf_counter()
     print('Run', end - start, 's')
